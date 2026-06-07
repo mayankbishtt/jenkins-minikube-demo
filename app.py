7c2edd1490db69efdaa8179abfa5f568579d5bee
Mayank Bisht <mayankbisht2023@gmail.com>
2026-06-08 02:04:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+print("Application Started")
+
 @app.route("/")
 def home():
-    return "Hello from Jenkins"
+    print("Request received")
+    return "Hello from Jenkins and Kubernetes"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
